3aa1ebe21c9edf9da9d6976dc499d5fdaf3551d4
Alexey Svetlyakov <devpieron@gmail.com>
2020-03-09 23:11:53 +0100

    Make it work with webpack 5

--- cordovaLibLoader.js
+++ cordovaLibLoader.js
@@ -6,16 +6,17 @@ const defineRegexp = /define([,;])/g;
 // cordova_b.js assigns but does not define cordova.define()
 const cordovaExportRegexp = /module\.exports\s*=\s*cordova\s*;/;
 
-const defineStub = "(function (){ function stub() {}; stub.remove = function () {}; return stub; })()"
+const defineStub =
+  "(function (){ function stub() {}; stub.remove = function () {}; return stub; })()";
 
 /**
  * @param {string} source
  */
 module.exports = function cordovaLoader(source) {
-    source = source
-        .replace(clobbersRegexp, "clobbers(require.resolve('$1'),")
-        .replace(requireRegexp, "__webpack_require__$1")
-        .replace(defineRegexp, defineStub + "$1")
-        .replace(cordovaExportRegexp, "window.cordova = module.exports = cordova;");
-    return source;
-}
+  source = source
+    .replace(clobbersRegexp, "clobbers(require.resolve('$1'),")
+    .replace(requireRegexp, "__webpack_require__$1")
+    .replace(defineRegexp, defineStub + "$1")
+    .replace(cordovaExportRegexp, "window.cordova = module.exports = cordova;");
+  return source;
+};
--- cordovaPluginListLoader.js
+++ cordovaPluginListLoader.js
@@ -1,47 +1,59 @@
 // @ts-check
 const path = require("path");
+
 const processPluginXML = require("./utils");
 
-
 module.exports = async function cordovaPluginsLoader(emptySource) {
-    // @ts-ignore
-    const callback = this.async();
-    /** @type {string[]} */
-    // @ts-ignore
-    const plugins = this.query.plugins || [];
-    // @ts-ignore
-    const platform = this.query.platform;
-    if (!platform) {
-        return callback(new Error("Platform was not specified"));
-    }
-    if (!plugins.length) {
-        return callback(null, emptySource);
-    }
-    const pluginList = [];
-    for (const plugin of plugins) {
-        try {
-            const pluginPath = require.resolve(path.join(plugin, "./plugin.xml"));
-            const files = await processPluginXML(pluginPath, platform);
-            if (!files.length) {
-                continue;
-                // return callback(null, emptySource);
-            }
-            pluginList.push(...files.map(f => {
-                // since it's streated as source and we need require.resolve without quotes, build string manually instead of JSON.stringify()
-                const str = `{
+  // @ts-ignore
+  const callback = this.async();
+  /** @type {string[]} */
+  // @ts-ignore
+  const plugins = this.query.plugins || [];
+  // @ts-ignore
+  const platform = this.query.platform;
+  if (!platform) {
+    return callback(new Error("Platform was not specified"));
+  }
+  if (!plugins.length) {
+    return callback(null, emptySource);
+  }
+  const pluginList = [];
+  for (const plugin of plugins) {
+    try {
+      const pluginPath = require.resolve(path.join(plugin, "./plugin.xml"));
+      const files = await processPluginXML(pluginPath, platform);
+      if (!files.length) {
+        continue;
+        // return callback(null, emptySource);
+      }
+      pluginList.push(
+        ...files.map(f => {
+          // since it's streated as source and we need require.resolve without quotes, build string manually instead of JSON.stringify()
+          const str = `{
                     "id": require.resolve('${plugin}.${f.name}'),
                     "file": require.resolve('${plugin}.${f.name}'),
                     "pluginId": "${plugin}",
-                    ${f.clobbers.length ? `"clobbers": ${JSON.stringify(f.clobbers)},` : ""}
-                    ${f.merges.length ? `"merges": ${JSON.stringify(f.merges)},` : ""}
-                    ${f.runs ? `"runs": true,` : ""}
+                    ${
+                      f.clobbers.length
+                        ? `"clobbers": ${JSON.stringify(f.clobbers)},`
+                        : ""
+                    }
+                    ${
+                      f.merges.length
+                        ? `"merges": ${JSON.stringify(f.merges)},`
+                        : ""
+                    }
+                    ${f.runs ? '"runs": true,' : ""}
                 }`;
-                return str;
-            }));
-        } catch (e) {
-            return callback(e);
-        }
+          return str;
+        })
+      );
+    } catch (e) {
+      return callback(e);
     }
-    const newSource = `module.exports = [${pluginList.join(",")}]; module.exports.metadata = {};`;
-    return callback(null, newSource);
-}
+  }
+  const newSource = `module.exports = [${pluginList.join(
+    ","
+  )}]; module.exports.metadata = {};`;
+  return callback(null, newSource);
+};
